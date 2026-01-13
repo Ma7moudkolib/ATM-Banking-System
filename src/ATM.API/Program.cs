2d6c78696be5a33efc865dd9f67d0eb28320c3fb
Mahmoud Kolib <mahmoudkolib22@gmial.com>
2026-01-14 00:52:35 +0200

    chore(api): register application services

--- src/ATM.API/Program.cs
+++ src/ATM.API/Program.cs
@@ -1,18 +1,81 @@
+using ATM.API.Middleware;
+using ATM.Application.Interfaces.Repositories;
+using ATM.Application.Interfaces.Services;
+using ATM.Application.Services;
+using ATM.Infrastructure.Context;
+using ATM.Infrastructure.Repositories;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
-
+builder.Services.AddApiVersioning(options =>
+{
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.ReportApiVersions = true;
+});
+builder.Services.AddVersionedApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'VVV";
+    options.SubstituteApiVersionInUrl = true;
+});
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AtmDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        b => b.MigrationsAssembly("AtmApi.Infrastructure")
+    )
+);
+builder.Services.AddMemoryCache();
+
+builder.Services.AddScoped<IRepositoryManager, RepositoryManager>();
+
+builder.Services.AddScoped<IServiceManager, ServiceManager>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new()
+    {
+        Title = "ATM API",
+        Version = "v1",
+        Description = "A secure ATM API following Clean Architecture principles"
+    });
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
-
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
