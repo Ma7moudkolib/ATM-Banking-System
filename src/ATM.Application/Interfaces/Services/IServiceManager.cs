bc7b85d80c7ceb7431a6a93c9a268dba4b069f65
Mahmoud Kolib <mahmoudkolib22@gmial.com>
2026-01-13 23:14:32 +0200

    feat(application): add service manager

---
+++ src/ATM.Application/Interfaces/Services/IServiceManager.cs
@@ -0,0 +1,10 @@
+﻿namespace ATM.Application.Interfaces.Services
+{
+    public interface IServiceManager
+    {
+        IAuthenticationService Authentication { get; }
+        IAccountService Account { get; }
+        ITransactionService Transaction { get; }
+        IAuditService Audit { get; }
+    }
+}
---
+++ src/ATM.Application/Services/ServiceManager.cs
@@ -0,0 +1,28 @@
+﻿using ATM.Application.Interfaces.Repositories;
+using ATM.Application.Interfaces.Services;
+
+namespace ATM.Application.Services
+{
+    public class ServiceManager : IServiceManager
+    {
+        private readonly Lazy<IAuthenticationService> _authenticationService;
+        private readonly Lazy<IAccountService> _accountService;
+        private readonly Lazy<ITransactionService> _transactionService;
+        private readonly Lazy<IAuditService> _auditService;
+
+        public ServiceManager(
+            IRepositoryManager repository,
+            Microsoft.Extensions.Caching.Memory.IMemoryCache cache)
+        {
+            _auditService = new Lazy<IAuditService>(() => new AuditService(repository));
+            _authenticationService = new Lazy<IAuthenticationService>(() => new AuthenticationService(repository, cache));
+            _accountService = new Lazy<IAccountService>(() => new AccountService(repository, _authenticationService.Value));
+            _transactionService = new Lazy<ITransactionService>(() => new TransactionService(repository, _authenticationService.Value, _auditService.Value));
+        }
+
+        public IAuthenticationService Authentication => _authenticationService.Value;
+        public IAccountService Account => _accountService.Value;
+        public ITransactionService Transaction => _transactionService.Value;
+        public IAuditService Audit => _auditService.Value;
+    }
+}
