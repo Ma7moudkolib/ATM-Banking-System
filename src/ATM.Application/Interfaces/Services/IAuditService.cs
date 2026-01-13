923d3167034c4dc5a5d38bbc295463f4dda226e3
Mahmoud Kolib <mahmoudkolib22@gmial.com>
2026-01-13 23:13:53 +0200

    feat(application): add audit service

---
+++ src/ATM.Application/Interfaces/Services/IAuditService.cs
@@ -0,0 +1,7 @@
+﻿namespace ATM.Application.Interfaces.Services
+{
+    public interface IAuditService
+    {
+        Task LogAsync(string entityType, int entityId, string action, string changes, string performedBy, string ipAddress);
+    }
+}
---
+++ src/ATM.Application/Services/AuditService.cs
@@ -0,0 +1,23 @@
+﻿using ATM.Application.Interfaces.Repositories;
+using ATM.Application.Interfaces.Services;
+using ATM.Domain.Entities;
+
+namespace ATM.Application.Services
+{
+    public class AuditService : IAuditService
+    {
+        private readonly IRepositoryManager _repository;
+
+        public AuditService(IRepositoryManager repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task LogAsync(string entityType, int entityId, string action, string changes, string performedBy, string ipAddress)
+        {
+            var auditLog = new AuditLog(entityType, entityId, action, changes, performedBy, ipAddress);
+            await _repository.AuditLog.AddAsync(auditLog);
+            await _repository.UnitOfWork.SaveChangesAsync();
+        }
+    }
+}
